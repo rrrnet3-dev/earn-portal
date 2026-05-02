7a8bee26caffcffeb97252b5333cbd2236b63188
ravimeka79 <ravimeka79@gmail.com>
2026-05-02 15:49:47 +0530

    v2.2 PWA complete

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from 'next'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+}
+
+export default withPWA(nextConfig)
