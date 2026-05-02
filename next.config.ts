dba59163424770826f5e547502e9a233c0af3700
ravimeka79 <ravimeka79@gmail.com>
2026-05-02 16:17:36 +0530

    Remove PWA, stable v2.2 build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next'
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-})
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
